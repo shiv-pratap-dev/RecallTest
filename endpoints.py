d9983cfdeb953e1ddc0a676f3e3c260ab21e58be
Shiv Pratap Singh <shivpratapsingh@Shivs-MacBook-Air.local>
2026-03-31 21:35:31 +0530

    basic idea plan

---
+++ endpoints.py
@@ -0,0 +1,30 @@
+#  run for 1 image
+#  user uploads a photo
+#  run ocr + image recog
+#  res(ocr) = make embeddings of those , store it in  faiss vector store 
+#  run imge recog, object detection using yolo
+#  store in faiss
+#  use an embedder 
+#  imp; have an image id, dono ki ek image , dono stored under one folder
+stored_image{
+    "imageid" : "uuid", # MUST
+    "text_emd" : [[whdhdshc]], # ocr
+    "image_emd" : [[whdhdshc]], # faiss
+    "object details": 'deatils' = {
+        'color';
+        size,
+        eyes color
+        text available: ['all text retrived']
+    }
+}
+
+#  take original image using image id and store it in cloudfare contain :
+image = {
+    image url,
+    image_id;
+    basic_details;
+
+}
+
+
+#  connect these two 
